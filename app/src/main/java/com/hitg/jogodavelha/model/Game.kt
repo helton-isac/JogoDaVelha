ac40b160e803bfe1602da96c1e78a397883c0261
Helton Isac <helton.isac@gmail.com>
2020-11-05 20:30:37 -0300

    TOC

--- app/src/main/java/com/hitg/jogodavelha/model/Game.kt
+++ app/src/main/java/com/hitg/jogodavelha/model/Game.kt
@@ -4,15 +4,23 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 
 class Game(playerOne: String, playerTwo: String) {
+
+    companion object {
+        private val TAG = Game::class.java.simpleName
+        private val BOARD_SIZE = 3
+    }
+
     var player1: Player? = null
     var player2: Player? = null
     var currentPlayer = player1
     var cells: Array<Array<Cell?>>
     var winner: MutableLiveData<Player> = MutableLiveData()
 
-    companion object {
-        private val TAG = Game::class.java.simpleName
-        private val BOARD_SIZE = 3
+    init {
+        cells = Array(BOARD_SIZE) { arrayOfNulls<Cell>(BOARD_SIZE) }
+        player1 = Player(playerOne, "x")
+        player2 = Player(playerTwo, "o")
+        currentPlayer = player1
     }
 
     val isBoardFull: Boolean
@@ -23,12 +31,6 @@ class Game(playerOne: String, playerTwo: String) {
             return true
         }
 
-    init {
-        cells = Array(BOARD_SIZE) { arrayOfNulls<Cell>(BOARD_SIZE) }
-        player1 = Player(playerOne, "x")
-        player2 = Player(playerTwo, "o")
-        currentPlayer = player1
-    }
 
     fun switchPlayer() {
         currentPlayer = if (currentPlayer === player1) player2 else
@@ -43,21 +45,27 @@ class Game(playerOne: String, playerTwo: String) {
     }
 
     private fun areEqual(vararg cells: Cell?): Boolean {
-        for (cell in cells) if (cell?.player?.value == null)
-            return false
-        val comparisonBase = cells[0]
-        for (i in 1 until cells.size)
-            if
-                    (!comparisonBase?.player?.value.equals(cells[i]?.player?.value))
+        for (cell in cells) {
+            if (cell?.player?.value == null) {
                 return false
+            }
+        }
+        val comparisonBase = cells[0]
+        for (i in 1 until cells.size) {
+            if (!comparisonBase?.player?.value.equals(cells[i]?.player?.value)) {
+                return false
+            }
+        }
         return true
     }
 
     fun hasThreeSameHorizontalCells(): Boolean {
         try {
-            for (i in 0 until BOARD_SIZE)
-                if (areEqual(cells[i][0], cells[i][1], cells[i][2]))
+            for (i in 0 until BOARD_SIZE) {
+                if (areEqual(cells[i][0], cells[i][1], cells[i][2])) {
                     return true
+                }
+            }
             return false
         } catch (e: NullPointerException) {
             Log.e(TAG, e.message!!)
