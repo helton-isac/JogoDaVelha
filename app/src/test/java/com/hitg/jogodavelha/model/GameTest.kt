f9e50b378908b444e6ec35947ec6134e082139d8
Helton Isac <helton.isac@gmail.com>
2020-11-05 21:07:03 -0300

    More Junit5

--- app/src/test/java/com/hitg/jogodavelha/model/GameTest.kt
+++ app/src/test/java/com/hitg/jogodavelha/model/GameTest.kt
@@ -1,19 +1,65 @@
 package com.hitg.jogodavelha.model
 
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 
 class GameTest {
 
+    private lateinit var game: Game
+    private var player: Player? = null
+    private var anotherPlayer: Player? = null
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        game = Game("Heider", "Android")
+        player = game.player1
+        anotherPlayer = game.player2
+    }
+
     @Test
     fun returnTrueIfHasThreeSameHorizontalCellsAtRow1() {
-        val game = Game("Heider", "Android")
-        val player = game.player1
         val cell = Cell(player)
         game.cells[0][0] = cell
         game.cells[0][1] = cell
         game.cells[0][2] = cell
         val hasThreeSameHorizontalCells = game.hasThreeSameHorizontalCells()
         assertTrue(hasThreeSameHorizontalCells)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun returnTrueIfHasThreeSameHorizontalCellsAtRow2() {
+        val cell = Cell(player)
+        game.cells[1][0] = cell
+        game.cells[1][1] = cell
+        game.cells[1][2] = cell
+        val hasThreeSameHorizontalCells = game.hasThreeSameHorizontalCells()
+        assertTrue(hasThreeSameHorizontalCells)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun returnTrueIfHasThreeSameHorizontalCellsAtRow3() {
+        val cell = Cell(player)
+        game.cells[2][0] = cell
+        game.cells[2][1] = cell
+        game.cells[2][2] = cell
+        val hasThreeSameHorizontalCells = game.hasThreeSameHorizontalCells()
+        assertTrue(hasThreeSameHorizontalCells)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun returnFalseIfDoesNotHaveThreeSameHorizontalCells() {
+        val cell = Cell(player)
+        val anotherCell = Cell(anotherPlayer)
+        game.cells[0][0] = cell
+        game.cells[0][1] = cell
+        game.cells[0][2] = anotherCell
+        val hasThreeSameHorizontalCells = game.hasThreeSameHorizontalCells()
+        assertFalse(hasThreeSameHorizontalCells)
     }
 }
--- app/src/test/java/com/hitg/jogodavelha/model/GameTestJU5.kt
+++ app/src/test/java/com/hitg/jogodavelha/model/GameTestJU5.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
+@DisplayName("GameTest with jUnit5")
 @Nested
 class GameTestJU5 {
 
@@ -21,8 +22,8 @@ class GameTestJU5 {
         }
 
         @Nested
-        @DisplayName("When a player does three cells in a row")
-        inner class SetThreeCellsInARow() {
+        @DisplayName("When a player does three cells in the first row")
+        inner class SetThreeCellsInTheFirstRow() {
 
             @BeforeEach
             fun beforeEach() {
@@ -31,6 +32,27 @@ class GameTestJU5 {
                 game.cells[0][0] = cell
                 game.cells[0][1] = cell
                 game.cells[0][2] = cell
+            }
+
+            @DisplayName("Then the hasThreeSameHorizontalCells should return true")
+            @Test
+            fun checkHasThreeSameHorizontalCells() {
+                val hasThreeSameHorizontalCells = game.hasThreeSameHorizontalCells()
+                Assert.assertTrue(hasThreeSameHorizontalCells)
+            }
+        }
+
+        @Nested
+        @DisplayName("When a player does three cells in the second row")
+        inner class SetThreeCellsInTheSecondRow() {
+
+            @BeforeEach
+            fun beforeEach() {
+                val player = game.player1
+                val cell = Cell(player)
+                game.cells[1][0] = cell
+                game.cells[1][1] = cell
+                game.cells[1][2] = cell
             }
 
             @DisplayName("Then the hasThreeSameHorizontalCells should return true")
