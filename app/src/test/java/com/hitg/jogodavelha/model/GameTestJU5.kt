33f3769666ff4ad46306ba085b8b09bd4a262047
Helton Isac <helton.isac@gmail.com>
2020-11-05 21:35:32 -0300

    Updating JUnit5 Test

--- app/src/test/java/com/hitg/jogodavelha/model/GameTestJU5.kt
+++ app/src/test/java/com/hitg/jogodavelha/model/GameTestJU5.kt
@@ -15,20 +15,23 @@ class GameTestJU5 {
     class SetGame() {
 
         lateinit var game: Game
+        lateinit var player1: Player
+        lateinit var player2: Player
 
         @BeforeEach
         fun beforeEach() {
             game = Game("Heider", "Android")
+            player1 = game.player1!!
+            player2 = game.player2!!
         }
 
         @Nested
-        @DisplayName("When a player does three cells in the first row")
+        @DisplayName("When a player has three cells in the first row")
         inner class SetThreeCellsInTheFirstRow() {
 
             @BeforeEach
             fun beforeEach() {
-                val player = game.player1
-                val cell = Cell(player)
+                val cell = Cell(player1)
                 game.cells[0][0] = cell
                 game.cells[0][1] = cell
                 game.cells[0][2] = cell
@@ -43,13 +46,12 @@ class GameTestJU5 {
         }
 
         @Nested
-        @DisplayName("When a player does three cells in the second row")
+        @DisplayName("When a player has three cells in the second row")
         inner class SetThreeCellsInTheSecondRow() {
 
             @BeforeEach
             fun beforeEach() {
-                val player = game.player1
-                val cell = Cell(player)
+                val cell = Cell(player1)
                 game.cells[1][0] = cell
                 game.cells[1][1] = cell
                 game.cells[1][2] = cell
@@ -60,6 +62,47 @@ class GameTestJU5 {
             fun checkHasThreeSameHorizontalCells() {
                 val hasThreeSameHorizontalCells = game.hasThreeSameHorizontalCells()
                 Assert.assertTrue(hasThreeSameHorizontalCells)
+            }
+        }
+
+        @Nested
+        @DisplayName("When a player has three cells in the last row")
+        inner class SetThreeCellsInTheLastRow() {
+
+            @BeforeEach
+            fun beforeEach() {
+                val cell = Cell(player1)
+                game.cells[2][0] = cell
+                game.cells[2][1] = cell
+                game.cells[2][2] = cell
+            }
+
+            @DisplayName("Then the hasThreeSameHorizontalCells should return true")
+            @Test
+            fun checkHasThreeSameHorizontalCells() {
+                val hasThreeSameHorizontalCells = game.hasThreeSameHorizontalCells()
+                Assert.assertTrue(hasThreeSameHorizontalCells)
+            }
+        }
+
+        @Nested
+        @DisplayName("When a player has only two cells in a row")
+        inner class SetAThreeCellsInTheLastRow() {
+
+            @BeforeEach
+            fun beforeEach() {
+                val cell = Cell(player1)
+                val anotherCell = Cell(player2)
+                game.cells[2][0] = cell
+                game.cells[2][1] = cell
+                game.cells[2][2] = anotherCell
+            }
+
+            @DisplayName("Then the hasThreeSameHorizontalCells should return false")
+            @Test
+            fun checkHasThreeSameHorizontalCells() {
+                val hasThreeSameHorizontalCells = game.hasThreeSameHorizontalCells()
+                Assert.assertFalse(hasThreeSameHorizontalCells)
             }
         }
     }
